661dfdadeba702f68ef238bfe2d652cd864c8868
Doan Minh Toan <aatroxlg2005@gmail.com>
2026-06-11 10:25:49 +0800

    Remove SQLite import from server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
-const db = require('./db/database');
+
 
 const app = express();
 const PORT = 3001;
